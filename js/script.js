e1df3a1a55b0f7c4132ac24591be50718a5a582d
Matheus <matheusbarrosohomeoffice@gmail.com>
2025-06-05 13:29:06 -0300

    Adicionando verificação de campos obrigatorios nos formulários

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ document.getElementById('prev').addEventListener('click', () => {
   changeSlide(-1);
 });
 
-function changeSlide(direction) {
+const changeSlide = (direction) => {
   slides[currentSlide].classList.remove('active');
   currentSlide = (currentSlide + direction + slides.length) % slides.length;
   slides[currentSlide].classList.add('active');
@@ -20,3 +20,21 @@ function changeSlide(direction) {
 setInterval(() => {
   changeSlide(1);
 }, 5000);
+
+// Validação de Formulário
+const form = document.getElementById('cadastroForm');
+
+form.addEventListener('submit', (event) => {
+  const nome = document.getElementById('nome').value.trim();
+  const email = document.getElementById('email').value.trim();
+  const cidade = document.getElementById('cidade').value.trim();
+  const cep = document.getElementById('cep').value.trim();
+
+  if (!nome || !email || !cidade || !cep) {
+    event.preventDefault(); // Impede o envio
+    return alert('Por favor, preencha todos os campos obrigatórios.');
+  }
+
+  
+});
+
